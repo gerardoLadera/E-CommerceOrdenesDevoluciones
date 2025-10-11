b8e931164f8c2cbf11466947381053e2fcb7e172
GDaniel <gerardo.ladera@unmsm.edu.pe>
2025-10-11 03:43:49 -0500

    feat:config module for inventory mock

--- Backend/inventory-mock/src/inventory.mock.module.ts
+++ Backend/inventory-mock/src/inventory.mock.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { InventoryMockController } from './inventory.mock.controller';
 import { InventoryMockService } from './inventory.mock.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+      isGlobal: true,
+    }),
+  ],
   controllers: [InventoryMockController],
   providers: [InventoryMockService],
 })
