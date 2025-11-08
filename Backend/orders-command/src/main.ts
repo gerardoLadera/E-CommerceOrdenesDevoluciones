01daab7bf6a16482f1a545237b9c34a41417e1f4
jose-lv <12200058@unmsm.edu.pe>
2025-11-08 01:01:09 -0500

    cicd1.2

--- Backend/orders-command/src/main.ts
+++ Backend/orders-command/src/main.ts
@@ -6,22 +6,26 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Elimina propiedades no incluidas en el DTO
-    forbidNonWhitelisted: true, // Lanza error si hay propiedades no permitidas
-    transform: true, // Transforma tipos automáticamente
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propiedades no incluidas en el DTO
+      forbidNonWhitelisted: true, // Lanza error si hay propiedades no permitidas
+      transform: true, // Transforma tipos automáticamente
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Orders Command API')
-    .setDescription('Sistema de gestión de órdenes y devoluciones - E-Commerce Deportivo')
+    .setDescription(
+      'Sistema de gestión de órdenes y devoluciones - E-Commerce Deportivo',
+    )
     .setVersion('1.0')
     .addTag('orders', 'Operaciones relacionadas con órdenes')
     .addTag('returns', 'Operaciones relacionadas con devoluciones')
     .setContact('Equipo 2', '', 'carlos.montenegro4@unmsm.edu.pe')
     .setLicense('MIT', 'https://opensource.org/licenses/MIT')
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document, {
     customSiteTitle: 'API Documentation - Orders Service',
@@ -29,14 +33,15 @@ async function bootstrap() {
     explorer: true, // Permitir búsqueda en la documentación
   });
 
-
   // console.log('Swagger docs available at http://localhost:3001/api-docs');
   console.log('Swagger docs available at /api-docs');
-  const port = process.env.PORT;
+  const port = process.env.PORT || 8080;
+  /*
   if (!port) {
     throw new Error('PORT environment variable is not defined');
-  }
-  await app.listen(parseInt(port, 10));
+  }*/
+  //await app.listen(parseInt(port, 10));
+  await app.listen(port);
   console.log(`Orders Command Service running on port ${process.env.PORT}`);
   console.log('Kafka microservice connected');
 }
