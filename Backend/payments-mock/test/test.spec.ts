1abcf0f3ed0bf27bfb6487230532e32be6ce6418
jose-lv <12200058@unmsm.edu.pe>
2025-11-08 12:18:19 -0500

    cicd

--- Backend/payments-mock/test/test.spec.ts
+++ Backend/payments-mock/test/test.spec.ts
@@ -1,7 +1,7 @@
 describe('Initial Test Suite', () => {
   // Una prueba simple para asegurar que Jest se está ejecutando correctamente  para el CICD
   it('should pass if the test environment is correctly configured', () => {
-    // Esto es un marcador de posición. Asegura que Jest encuentre y ejecute al menos una prueba.
+    // Esto es un marcador de posición. Asegura que Jest encuentre y ejecute al menos una prueba.a
     expect(true).toBe(true);
   });
 });
--- Backend/returns/test/test.spec.ts
+++ Backend/returns/test/test.spec.ts
@@ -1,6 +1,6 @@
 describe('Initial Test Suite', () => {
   it('should pass if the test environment is correctly configured', () => {
-    // Prueba
+    // Pruebaa
     expect(true).toBe(true);
   });
 });
