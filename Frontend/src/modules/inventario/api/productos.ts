a77d7f928604599a88ebadccc1c9cf2725d409b6
GDaniel <gerardo.ladera@unmsm.edu.pe>
2025-11-08 03:04:17 -0500

    feat:delete frontend file

--- Frontend/src/modules/inventario/api/productos.ts
+++
@@ -1,7 +0,0 @@
-import API from "./api";
-import type { Producto } from "../types/producto";
-
-export const getProductos = async (): Promise<Producto[]> => {
-  const { data } = await API.get<Producto[]>("/productos");
-  return data;
-};
--- Frontend/src/types/ui/NavItem.d.ts
+++
@@ -1,6 +0,0 @@
-export type NavItem = {
-  label: string;
-  path?: string;
-  icon?: React.ReactNode;
-  children?: { label: string; path: string }[];
-}
