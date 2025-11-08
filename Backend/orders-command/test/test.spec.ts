a4e7b077529f2f104e001dc72acf6cd57deb7172
jose-lv <12200058@unmsm.edu.pe>
2025-11-08 00:03:33 -0500

    orders

--- Backend/orders-command/test/test.spec.ts
+++ Backend/orders-command/test/test.spec.ts
@@ -1,6 +1,6 @@
 describe('Initial Test Suite', () => {
   it('should pass if the test environment is correctly configured', () => {
-    // Prueba de humo para el CICD 4
+    // Prueba de humo para el CICD5
     expect(true).toBe(true);
   });
 });
