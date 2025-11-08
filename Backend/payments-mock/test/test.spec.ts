ebf29c8950ad4e88f677dab19e23ba0b5aa703be
jose-lv <12200058@unmsm.edu.pe>
2025-11-08 02:20:23 -0500

    cicd1.4

--- Backend/payments-mock/test/test.spec.ts
+++ Backend/payments-mock/test/test.spec.ts
@@ -1,5 +1,5 @@
 describe('Initial Test Suite', () => {
-  // Una prueba simple para asegurar que Jest se está ejecutando correctamente  para el CI
+  // Una prueba simple para asegurar que Jest se está ejecutando correctamente  para el CICD
   it('should pass if the test environment is correctly configured', () => {
     // Esto es un marcador de posición. Asegura que Jest encuentre y ejecute al menos una prueba.
     expect(true).toBe(true);
